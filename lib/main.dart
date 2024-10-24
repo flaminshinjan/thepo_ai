570bf28408fde4f18f7b8390e062dad9cb728e38
flaminshinjan <srikpatra.2008@gmail.com>
2024-10-24 13:48:03 +0530

    feat: app icon added

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'The Po.ai',
       debugShowCheckedModeBanner: false,
       home: BlocProvider(
         create: (context) => ConversationBloc(),
